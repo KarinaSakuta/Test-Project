1f97ca75e90c865ce6ba55137b486ca511dcd896
Karina <47184215+KarinaSakuta@users.noreply.github.com>
2020-03-01 20:01:29 +0300

    Updates

---
+++ precache-manifest.e3fb93593901b259aae9e67e53c53c63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "debebb1d37f31114a2a02beb84a0ef23",
+    "url": "/Test-Project/index.html"
+  },
+  {
+    "revision": "c25caa9b04b11b4c8eac",
+    "url": "/Test-Project/static/css/main.767961a9.chunk.css"
+  },
+  {
+    "revision": "e141aa9faf680f4ec5f4",
+    "url": "/Test-Project/static/js/2.f3d9ab5f.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/Test-Project/static/js/2.f3d9ab5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c25caa9b04b11b4c8eac",
+    "url": "/Test-Project/static/js/main.4e68db34.chunk.js"
+  },
+  {
+    "revision": "8c63ea1cb02d4cac9a8b",
+    "url": "/Test-Project/static/js/runtime-main.d2704d6a.js"
+  },
+  {
+    "revision": "9b034cbc58c86b5dfa30edba35b81ea6",
+    "url": "/Test-Project/static/media/spinner.9b034cbc.png"
+  }
+]);
